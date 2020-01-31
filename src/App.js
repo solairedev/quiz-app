336dcf04f7f02e8db8f3c29fc1b65b1c2b52c738
solaire-off <work.victor.ananev@yandex.ru>
2020-01-31 23:11:47 +0300

    Added base quiz layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="quiz">
+        <div className="quiz__wrap">
+          <div className="quiz__header">
+            <p className="quiz__title">
+              Which property is used to change the background color?
+            </p>
+
+            <p className="quiz__meta">
+              Question 1/10
+            </p>
+          </div>
+          <div className="quiz__answer-list">
+            <button className="quiz__answer btn">
+              color
+            </button>
+            <button className="quiz__answer btn">
+              bgcolor
+            </button>
+            <button className="quiz__answer quiz__answer--danger btn">
+              background-color
+            </button>
+            <button className="quiz__answer quiz__answer--success btn">
+              bgColor
+            </button>
+          </div>
+          <div className="quiz__action">
+            <button className="btn quiz__action-btn btn--accent">Next</button>
+          </div>
+        </div>
+      </div>
+      <footer className="footer">
+        Questions source: <a className="App-link" href="https://opentdb.com/" target="_blank" rel="noopener noreferrer" >Open Trivia Database</a>
+      </footer>
     </div>
   );
 }
