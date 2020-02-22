ad4cc7e5a67c1587e471e168b8d4f022a4c81e9f
solaire-off <work.victor.ananev@yandex.ru>
2020-02-23 02:20:06 +0300

    Saving data in localstorage

--- src/store.js
+++ src/store.js
@@ -1,9 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index';
 import thunk from 'redux-thunk';
+import { saveState, loadState } from './localStorage';
+import throttle from 'lodash/throttle';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const initState = loadState()
+
+const store = createStore(rootReducer, initState,  applyMiddleware(thunk));
 
 export default store;
 
+store.subscribe(throttle(() => {
+  saveState({
+    settings : store.getState().settings, 
+    questions : store.getState().questions, 
+  })
+}), 1000)
 
